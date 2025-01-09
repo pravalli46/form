e99b611ad260dc75741a5a8c420cc269b4a2d42a
pravalli46 <112859882+pravalli46@users.noreply.github.com>
2025-01-09 12:30:10 +0530

    Delete python directory

--- python/recursion.py
+++
@@ -1,8 +0,0 @@
-def multiply(x):
-    if x==1:
-        return x
-    else:
-       return x*multiply(x-1)
-x=int(input())
-result=multiply(x)
-print(result)
--- python/secondlargest.py
+++
@@ -1,9 +0,0 @@
-l=list(map(int,input().split()))
-first_largest=l[0]
-second_largest=l[0]
-for i in l:
-    if i>=first_largest:
-        second_largest=first_largest
-        first_largest=i
-print(second_largest)
-print(first_largest)
